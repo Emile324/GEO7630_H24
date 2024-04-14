f2ac004370d59b7e1c7c3c57f5ac3df77fb0b34e
antoupetit <141511068+antoupetit@users.noreply.github.com>
2024-04-14 14:32:29 +0000

    Push1

--- Atlas/app/Equipe6/code.js
+++ Atlas/app/Equipe6/code.js
@@ -1,12 +1,14 @@
-map.on('load',function(){
-    map.addSource('pistes_cyclables_agreables-source',{
-        type:'vector',
-        tiles:['https://automatic-waddle-g4q76xvr9x99fvprp-8801.app.github.dev/FH791176.pistes_cyclables_agreables/{z}/{x}/{y}.pbf']
-    })
-    map.addLayer({
-        'id':'pistes_cyclables_agreables',
-        'type':'fill',
-        'source':'pistes_cyclables_agreables-source',
-        'source-layer':'FH791176.pistes_cyclables_agreables'
-    })
+console.log("Le script code.js est chargé.");
+
+document.getElementById('Equipe6').addEventListener('click', function() {
+        map.addSource('pistes_cyclables_agreables-source', {
+            type: 'vector',
+            tiles: ['https://friendly-trout-jj5q9rp765q935g9j-8801.app.github.dev/FH791176.pistes_cyclables_agreables/{z}/{x}/{y}.pbf']
+        });
+        map.addLayer({
+            'id': 'pistes_cyclables_agreables',
+            'type': 'line',
+            'source': 'pistes_cyclables_agreables-source',
+            'source-layer': 'FH791176.pistes_cyclables_agreables'
+        });
 });
