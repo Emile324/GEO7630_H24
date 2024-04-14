30d6e92fe84e7e612b87686b21a2feee1c3067c6
antoupetit <141511068+antoupetit@users.noreply.github.com>
2024-04-14 15:01:56 +0000

    Affichage données

--- Atlas/app/Equipe6/code.js
+++ Atlas/app/Equipe6/code.js
@@ -1,17 +1,25 @@
 console.log("Le script code.js est chargé.");
 
-document.getElementById('Equipe6').addEventListener('click', function() {
-        map.addSource('pistes_cyclables_agreables-source', {
-            type: 'vector',
-            tiles: ['https://friendly-trout-jj5q9rp765q935g9j-8801.app.github.dev/FH791176.pistes_cyclables_agreables/{z}/{x}/{y}.pbf']
-        });
+document.getElementById('loadDataButton').addEventListener('click', function() {
+    addDataToMap();
+});
+
+function addDataToMap() {
+    // Ajoute la source
+    map.addSource('pistes_cyclables_agreables-source', {
+        type: 'vector',
+        tiles: ['https://friendly-trout-jj5q9rp765q935g9j-8801.app.github.dev/FH791176.pistes_cyclables_agreables/{z}/{x}/{y}.pbf'],
+        minzoom: 0,
+        maxzoom: 22
+    });
+
+    
+    // Ajoute la couche
         map.addLayer({
             'id': 'pistes_cyclables_agreables',
-            'type': 'line',
+            'type': 'line', // Assurez-vous que le type est approprié pour vos données.
             'source': 'pistes_cyclables_agreables-source',
             'source-layer': 'FH791176.pistes_cyclables_agreables'
         });
-});
- 
+}
 
-function(bizarre)ff
