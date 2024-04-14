21aec26aa83d42056200947a7ce81c28c4f00041
antoupetit <141511068+antoupetit@users.noreply.github.com>
2024-04-14 15:28:08 +0000

    Ajout zones de travaux

--- Atlas/app/Equipe6/code.js
+++ Atlas/app/Equipe6/code.js
@@ -1,25 +1,57 @@
 console.log("Le script code.js est chargé.");
 
-document.getElementById('loadDataButton').addEventListener('click', function() {
-    addDataToMap();
-});
+
+
+
 
 function addDataToMap() {
-    // Ajoute la source
+    // Ajouter les sources
+    if (map.getLayer('pistes_cyclables_agreables')) {
+        map.removeLayer('pistes_cyclables_agreables');
+    }
+    if (map.getSource('pistes_cyclables_agreables-source')) {
+        map.removeSource('pistes_cyclables_agreables-source');
+    }
+
+    if (map.getLayer('zone_travaux')) {
+        map.removeLayer('zone_travaux');
+    }
+    if (map.getSource('zone_travaux-source')) {
+        map.removeSource('zone_travaux-source');
+    }
+
     map.addSource('pistes_cyclables_agreables-source', {
         type: 'vector',
         tiles: ['https://friendly-trout-jj5q9rp765q935g9j-8801.app.github.dev/FH791176.pistes_cyclables_agreables/{z}/{x}/{y}.pbf'],
         minzoom: 0,
         maxzoom: 22
+     });
+
+    map.addSource('zone_travaux-source', {
+        type: 'vector',
+        tiles: ['https://friendly-trout-jj5q9rp765q935g9j-8801.app.github.dev/FH791176.zone_travaux/{z}/{x}/{y}.pbf'],
+        minzoom: 0,
+        maxzoom: 22
     });
+
 
     
     // Ajoute la couche
-        map.addLayer({
-            'id': 'pistes_cyclables_agreables',
-            'type': 'line', // Assurez-vous que le type est approprié pour vos données.
-            'source': 'pistes_cyclables_agreables-source',
-            'source-layer': 'FH791176.pistes_cyclables_agreables'
-        });
+    map.addLayer({
+       'id': 'pistes_cyclables_agreables',
+       'type': 'line', 
+       'source': 'pistes_cyclables_agreables-source',
+      'source-layer': 'FH791176.pistes_cyclables_agreables'
+   });
+
+    map.addLayer({
+        'id': 'zone_travaux',
+        'type': 'line',
+        'source': 'zone_travaux-source',
+        'source-layer': 'FH791176.zone_travaux'
+    });
 }
 
+document.getElementById('loadDataButton').addEventListener('click', function() {
+    addDataToMap();
+});
